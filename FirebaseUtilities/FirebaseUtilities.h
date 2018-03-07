cfd49eeed7d844fcff6846507b8d5af5f3fc4c1e
Jared Williams <jared@themovesapp.com>
2018-03-06 22:27:35 -0500

    Initial Commit

---
+++ FirebaseUtilities/FirebaseUtilities.h
@@ -0,0 +1,19 @@
+//
+//  FirebaseUtilities.h
+//  FirebaseUtilities
+//
+//  Created by Jared Williams on 3/6/18.
+//  Copyright © 2018 Jared Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirebaseUtilities.
+FOUNDATION_EXPORT double FirebaseUtilitiesVersionNumber;
+
+//! Project version string for FirebaseUtilities.
+FOUNDATION_EXPORT const unsigned char FirebaseUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseUtilities/PublicHeader.h>
+
+
